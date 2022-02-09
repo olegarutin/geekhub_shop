a08e8b02d6a3155a98702ee45fd518686161730c
oleg <o1eg.34ac@gmail.com>
2022-02-09 22:20:11 +0200

    Connect jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Entry point for the build script in your package.json
-import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-import './add_jquery'
+import Rails from '@rails/ujs'
+import '@hotwired/turbo-rails'
+import './controllers'
+import * as bootstrap from 'bootstrap'
 
 Rails.start()
