ac92ac4b817409f2ec6cda421c391d232841d0ea
oleg <o1eg.34ac@gmail.com>
2022-02-09 15:28:56 +0200

    Create models

--- db/migrate/20220209130959_create_products.rb
+++ db/migrate/20220209130959_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :tittle
+      t.string :title
       t.text :description
       t.decimal :price, precision: 10, scale: 2
       t.string :image
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_09_130959) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_09_132811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
-    t.string "tittle"
+    t.string "title"
     t.text "description"
     t.decimal "price", precision: 10, scale: 2
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "categories"
 end
