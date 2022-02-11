f49c12fba5e31389e533153e1b3e90efe25a0d44
oleg <o1eg.34ac@gmail.com>
2022-02-11 22:23:17 +0200

    Create routes for users and admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
   root 'products#index'
 
+  resources :users
   resources :categories, only: %i[show] do
     resources :products, only: %i[index show]
   end
