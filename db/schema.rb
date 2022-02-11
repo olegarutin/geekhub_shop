82aad42f3d0ffd6878b47942517f164c8d6ff836
oleg <o1eg.34ac@gmail.com>
2022-02-11 22:23:45 +0200

    Update table users

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_10_195609) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
