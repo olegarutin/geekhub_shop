1d6d7e04d7d5aa051f42c1c5107af64839d694b1
oleg <o1eg.34ac@gmail.com>
2022-02-13 17:58:23 +0200

    Add association with products and subcategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   validates :title, presence: true
 
   has_many :products, dependent: :destroy
+  has_many :subcategories, dependent: :destroy
 
   def to_param
     return nil unless persisted?
