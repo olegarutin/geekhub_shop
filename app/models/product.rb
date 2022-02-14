a4494fb572700b1887cd6687a4543ea63ec2871b
oleg <o1eg.34ac@gmail.com>
2022-02-14 23:34:30 +0200

    Add association

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :subcategory, optional: true
 
+  has_many :order_items
+
   validates :title, :description, :price, :image, presence: true
 end
