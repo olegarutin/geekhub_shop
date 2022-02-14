bdd9468cad08e401d04fa5fd27fa12b106b66673
oleg <o1eg.34ac@gmail.com>
2022-02-14 23:35:30 +0200

    Add helper current_order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :set_products_titles, :set_categories
+  helper_method :current_order
 
   SORTING_TYPE = { min: Product.order(price: :asc),
                    max: Product.order(price: :desc),
                    a_z: Product.order(title: :asc),
                    z_a: Product.order(title: :desc) }.freeze
+
+  def current_order
+    Order.find_or_create_by(user: current_user)
+  end
 
   private
 
