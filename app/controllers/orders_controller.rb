dfda4615df984400999e0a290b547a7b82fda90b
oleg <o1eg.34ac@gmail.com>
2022-02-18 23:33:17 +0200

    Add method to orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,8 @@ class OrdersController < ApplicationController
   end
 
   def update
-    # create a pass parameters method
-    current_order.update(full_name: params[:full_name], email: params[:email], phone: params[:phone], status: params[:status])
+    current_order.update(order_params)
+    OrderMailer.with(order: current_order).place_an_order.deliver_now
     current_order.destroy
     flash.alert = 'Order confirmed'
     redirect_to root_path
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
 
   private
 
-  # def order_params
-  #   params.require(:order).permit(:full_name, :email, :phone, :status)
-  # end
+  def order_params
+    params.permit(:full_name, :email, :phone, :status)
+  end
 end
