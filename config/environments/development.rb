0b9bc972f6def25f8856de5a4da2164226e88157
oleg <o1eg.34ac@gmail.com>
2022-02-18 23:36:50 +0200

    Add config for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            Rails.application.credentials.google_mail,
+    password:             Rails.application.credentials.google_secret_key,
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -69,4 +80,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
