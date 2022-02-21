60f64a8da55c3068d9b2954d409495c9942588c1
oleg <o1eg.34ac@gmail.com>
2022-02-21 20:03:39 +0200

    Add jquery-ujs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Entry point for the build script in your package.json
-import Rails from '@rails/ujs'
 import '@hotwired/turbo-rails'
 import './controllers'
 import * as bootstrap from 'bootstrap'
-
-Rails.start()
+import 'jquery-ujs'
