80f3a85ec909811f860350c56a1f78142f4b67bb
oleg <o1eg.34ac@gmail.com>
2022-02-22 13:34:13 +0200

    Fix total_price method

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,10 +6,6 @@ class Order < ApplicationRecord
   enum status: %i[in_progress ordered canceled]
 
   def total_price
-    sum = 0
-    self.order_items.each do |item|
-      sum += item.sub_price
-    end
-    return sum
+    order_items.map(&:sub_price).flatten.sum
   end
 end
