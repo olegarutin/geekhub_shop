c76f5e1f2cc06da3430a84f41e42ae5ad0e79616
oleg <o1eg.34ac@gmail.com>
2022-02-23 17:04:14 +0200

    Move constant to constans file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :set_products_titles, :set_categories
   helper_method :current_order
 
-  SORTING_TYPE = { min: Product.order(price: :asc),
-                   max: Product.order(price: :desc),
-                   a_z: Product.order(title: :asc),
-                   z_a: Product.order(title: :desc) }.freeze
-
   def current_order
     Order.find_or_create_by(user: current_user, status: 'in_progress')
   end
