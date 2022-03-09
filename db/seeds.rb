e625ebc0ad9237b9b1e894b38ae0e66fbbd44149
oleg <o1eg.34ac@gmail.com>
2022-03-09 20:49:51 +0200

    Fix seeds fike

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,11 @@ Category.create(title: 'Vegetables')
 User.create(admin: true, email: 'admin@gmail.com', password: 123456)
 
 12.times do
-  product = Product.create(title: Faker::Food.fruits, description: 'Fruits from Europe', price: Faker::Number.decimal(l_digits: 3), category_id: Category.all.sample.id)
+  product = Product.create(title: Faker::Food.fruits, description: 'Fruits from Europe', price: Faker::Number.decimal(l_digits: 3), category_id: 6)
   product.images.attach(io: File.open(File.join(Rails.root,'app/assets/images/fruits.jpg')), filename: 'fruits.jpg')
 end
+
 12.times do
-  product = Product.create(title: Faker::Food.vegetables, description: 'Vegetables from Europe', price: Faker::Number.decimal(l_digits: 3), category_id: Category.all.sample.id)
+  product = Product.create(title: Faker::Food.vegetables, description: 'Vegetables from Europe', price: Faker::Number.decimal(l_digits: 3), category_id: 7)
   product.images.attach(io: File.open(File.join(Rails.root,'app/assets/images/vegetables.jpg')), filename: 'vegetables.jpg')
 end
