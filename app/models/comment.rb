888d55a05ff52e5e679fbf85762ce0836d04b520
oleg <o1eg.34ac@gmail.com>
2022-03-21 20:52:15 +0200

    Remove useless validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,19 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  validates :body, :rating, presence: true
-  validates :user_id, uniqueness: { scope: :product, message: 'has already commented on this product' }
-  validate :user_ordered_product
+  validates :body, presence: true
 
   def rating_to_star
     '★' * rating.to_i + '☆' * (5 - rating.to_i)
-  end
-
-  private
-
-  def user_ordered_product
-    unless user.ordered_products.include?(product)
-      errors.add(:user, "didn't order this product")
-    end
   end
 end
