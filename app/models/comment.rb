b5482a1f1a94635cea4c8f8a5547d71f53c30bb4
oleg <o1eg.34ac@gmail.com>
2022-03-29 20:04:31 +0300

    user can leave only one comment with rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :product
 
   validates :body, presence: true
-  validates :rating, uniqueness: { message: 'has already rated on this product' }
+  validates :rating, presence: { scope: :user_id }, allow_nil: true
 
   def rating_to_star
     '★' * rating.to_i + '☆' * (5 - rating.to_i)
